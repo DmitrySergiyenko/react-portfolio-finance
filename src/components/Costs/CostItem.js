19091bbe2afaef2aaa788cdc1f7ab6b234f246d2
Dmitry Sergiyenko <dsergiyenko@astecindustries.com>
2022-05-31 16:48:16 -0500

    state

--- src/components/Costs/CostItem.js
+++ src/components/Costs/CostItem.js
@@ -1,17 +1,26 @@
+import { useState } from 'react';
 import './CostItem.css'
 import CostDate from './CostDate';
 import Card from '../UI/Card';
 
 const CostItem = (props) => {
 
+    useState(props.description);
+
+    const [description, setDescription] = useState(props.description);
+
+    const changeDescriptionHandker = () => {
+        setDescription("New item");
+    };
 
     return (
         <Card className='cost-item'>
             <CostDate date={props.date} />
             <div className='cost-item__description'>
-                <h2>{props.description}</h2>
+                <h2>{description}</h2>
                 <div className='cost-item__price'>${props.amount}</div>
             </div>
+            <button onClick={changeDescriptionHandker}>Change item</button>
         </Card>
     );
 }
