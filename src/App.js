b216177ed98e17fededb6b3a54d455f6141d86e8
Dmitry Sergiyenko <dsergiyenko@astecindustries.com>
2022-05-31 15:38:39 -0500

    basic idea

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import Costs from "./components/Costs/Costs";
 
-function App() {
+
+const App = () => {
+
+  const costs = [
+    {
+      date: new Date(2022, 2, 12),
+      description: "Fridge",
+      amount: 999.99
+    },
+    {
+      date: new Date(2022, 2, 25),
+      description: "MacBook",
+      amount: 1254.72
+    },
+    {
+      date: new Date(2022, 3, 8),
+      description: "Monitor",
+      amount: 249.99
+    }
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>React</h1>
+      <Costs costs={costs} />
     </div>
   );
 }
